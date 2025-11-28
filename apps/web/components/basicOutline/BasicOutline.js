63b0db72df207faf87883cd06131541611a7a58a
Jacob Wald <vug5ue@virginia.edu>
2025-11-28 00:26:08 -0500

    Navbar is now fixed

--- apps/web/components/basicOutline/BasicOutline.js
+++ apps/web/components/basicOutline/BasicOutline.js
@@ -1,6 +1,6 @@
 export default function BasicOutline({ children }) {
   return (
-    <div style={{ maxWidth: "100%", margin: "0 auto", padding: "20px" }}>
+    <div style={{ maxWidth: "100%", margin: "0 auto", padding: "20px", paddingTop: "80px" }}>
       {children}
     </div>
   );
